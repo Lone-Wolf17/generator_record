f93cb2727bb18ee2488a8b85e8b6f1bc70b5b727
Lone-Wolf17 <53178226+Lone-Wolf17@users.noreply.github.com>
2020-11-10 12:03:44 +0100

    ** Changed Package Name

--- android/app/src/main/kotlin/com/gmail/korex006/generator_record/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.gmail.korex006.generator_record
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/gmail/korex006/power_usage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gmail.korex006.power_usage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
